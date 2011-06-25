89bba28d2ea3c912ecd7b27bf837b2bdc201a2e9
Bertrand Paquet <bpaquet@octo.com>
2011-06-25 20:43:23 +0200

    Add logging

--- modules/vm.rb
+++ modules/vm.rb
@@ -7,7 +7,7 @@ end
 
 def reconfigure_all_net_device vm, opts
   vm.config.hardware.device.grep(VIM::VirtualEthernetCard).each do |eth|
-    puts "Reconfigure #{eth.deviceInfo.label} to #{opts[:type]}"
+    puts "Reconfigure #{eth.deviceInfo.label} to #{opts[:type]} for #{vm.name}"
     remove_device vm, eth.deviceInfo.label
     add_net_device vm, opts.merge(:network => eth.backing.network.name)
   end
